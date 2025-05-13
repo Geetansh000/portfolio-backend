a6c6a0f685543fbbb971f712e6f6cfaa86ba8c50
geetansh000 <gssharmakarnal0001@gmail.com>
2025-05-13 15:22:09 +0530

    Added database changes

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,8 @@ CORS_ALLOW_HEADERS =['Accept','Content-Type']# "access-control-allow-origin"
 CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins (you can restrict later if needed)
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",  # Allow the frontend Angular app
+    "portfolio-backend-cue6.onrender.com",
+    "*",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
