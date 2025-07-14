10b2947dea31f11139ea219a39636e095cd1ec20
geetansh000 <gssharmakarnal0001@gmail.com>
2025-07-14 10:38:40 +0530

    change cred

--- backend/settings.py
+++ backend/settings.py
@@ -84,10 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'postgres.flopfkrxftgjgfpfsesk',
         'PASSWORD': 'sharma',
-        'HOST': 'db.flopfkrxftgjgfpfsesk.supabase.co',#'localhost',
-        'PORT': '5432',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',#'localhost',
+        'PORT': '6543',
     }
 }
 
