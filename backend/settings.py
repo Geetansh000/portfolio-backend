32c46c588f767755210bb9d4b426547f3859c904
geetansh000 <gssharmakarnal0001@gmail.com>
2025-07-14 09:18:45 +0530

    change cred

--- backend/settings.py
+++ backend/settings.py
@@ -83,10 +83,10 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio_fdwn',
-        'USER': 'sharma',
-        'PASSWORD': 'loZPYr35m6hekbcd21nXMom4RorgTG9K',
-        'HOST': 'dpg-d0hh6sh5pdvs73egm4k0-a',#'localhost',
+        'NAME': 'portfolio_4xrq',
+        'USER': 'portfolio_4xrq_user',
+        'PASSWORD': '3T0QGlUt17FT1UsI4XPXdRyBkv74EKlQ',
+        'HOST': 'dpg-d1q7nqnfte5s73d1o1g0-a',#'localhost',
         'PORT': '5432',
     }
 }
