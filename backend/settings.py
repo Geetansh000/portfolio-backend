aa7b65cc36edf74d7bee5574254dfc2af903c52f
Geetansh <gssharmakarnal0001@gmail.com>
2025-08-03 14:15:06 +0530

    settings update

--- backend/settings.py
+++ backend/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '').split(',')]
-print (f"Allowed hosts: {ALLOWED_HOSTS}")
+ALLOWED_HOSTS = [host.strip()
+                 for host in os.getenv('ALLOWED_HOSTS', '').split(',')]
 
 # Application definition
 
@@ -39,16 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',  # CORS headers app
     'rest_framework',
     'rest_framework_simplejwt',
     'api',
     'user',
     'contacts',
     'projects',
-    'analytics'
+    'analytics',
 ]
 
+# Middleware
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # CORS middleware should be first
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,8 +61,22 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# CORS Settings
+# "access-control-allow-origin"
+CORS_ALLOW_HEADERS = ['Accept', 'Content-Type']
+
+# Allow all origins (you can restrict later if needed)
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [host.strip()
+                        for host in os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [host.strip()
+                      for host in os.getenv('CORS_ALLOW_METHODS', '').split(',')]
+# URL configuration
 ROOT_URLCONF = 'backend.urls'
 
+# Templates configuration
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -67,6 +84,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -115,13 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
